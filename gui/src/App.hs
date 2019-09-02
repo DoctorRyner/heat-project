0bd78610ccbe7223fc160aabec5b70e9249833b6
Ryner Gabriel <drryner@icloud.com>
2019-09-03 00:28:04 +0300

    Add frontend testing to gui

--- gui/src/App.hs
+++ gui/src/App.hs
@@ -1,9 +1,29 @@
 module App where
 
-data User = User { name, password :: String }
+import           Miso
 
-defaultUser :: User
-defaultUser = User "Keker" "deadspase2"
+data Model = Model {} deriving (Show, Eq)
 
-name' :: String
-name' = defaultUser.password
+defaultModel :: Model
+defaultModel = Model
+
+data Event
+    = NoEvent
+
+update :: Model -> Event -> Effect Event Model
+update model = \case
+    NoEvent -> pure model
+
+view :: Model -> View Event
+view _ = "Hello Haskell GUI ~"
+
+app :: App Model Event
+app = App
+    { initialAction = NoEvent
+    , model         = defaultModel
+    , update        = flip App.update
+    , view          = App.view
+    , events        = defaultEvents
+    , subs          = []
+    , mountPoint    = Nothing
+    }
