761cdae37c637b50186c4cdbc5697db54aa2941a
Ryner Gabriel <drryner@icloud.com>
2019-09-05 10:15:07 +0300

    Add better error support

--- gui/src/Http.hs
+++ gui/src/Http.hs
@@ -1,7 +1,7 @@
 module Http where
 
-import           Control.Exception
-import           Control.Monad.IO.Class      (liftIO)
+import           Control.Exception           (SomeException (..))
+import           Control.Monad               (liftM)
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8  as BSL
 import           JSDOM.Custom.XMLHttpRequest as JSDOM hiding (error)
@@ -34,15 +34,25 @@ post = POST
     , payload = Nothing
     }
 
+try a = catch (Right `liftM` a) (return . Left)
+
 send :: (FromJSON response, ToJSON response, ToJSON payload) => Request response payload -> JSM (Response response)
 send = \case
     GET url headers -> do
         req <- newXMLHttpRequest
         openSimple req ("GET" :: MisoString) url
         mapM_ (uncurry $ setRequestHeader req) headers
-        JSDOM.send req 
-        resRaw <- valToStr =<< getResponse req
-        case eitherDecodeStrict $ BSL.toStrict $ encode resRaw of
-            Right res -> pure $ Ok res
-            Left err  -> pure $ HttpError (ms err) 0
+        eitherXhrError <- try $ JSDOM.send req
+        case eitherXhrError of
+            Right _ -> do
+                resRaw <- valToStr =<< getResponse req
+                statusNum <- getStatus req
+                case eitherDecodeStrict $ BSL.toStrict $ encode resRaw of
+                    Right res -> do
+                        status <- getStatusText req :: JSM MisoString
+                        if status == "OK"
+                            then pure $ Ok res
+                            else pure $ HttpError (ms $ encode res) (fromEnum statusNum)
+                    Left err  -> pure $ HttpError (ms err) (fromEnum statusNum)
+            Left (SomeException _) -> pure $ HttpError "XHRError" 404
     POST url headers payload -> error ""
--- gui/src/Update.hs
+++ gui/src/Update.hs
@@ -4,7 +4,7 @@ module Update where
 --import           HttpReq
 import           Http
 import           Miso
---import           Miso.String (ms)
+import           Miso.String (ms)
 import           Types
 import           Utils
 
@@ -16,11 +16,11 @@ update model = \case
 --        pure $ PutNormalizeCss normalizeCss
     PutNormalizeCss resp -> case resp of
         Ok file -> pure $ model { files = model.files { normalizeCss = Just file } }
-        HttpError err _ -> model `withJS` do
-            alert err
+        HttpError err code -> model `withJS` do
+            alert $ err <> " | " <> ms (show code)
             pure NoEvent
     JSTest -> model `withJS` do
 --        x <- HttpReq.xhrGet ""
-        x <- Http.send $ get { url = "https://kurbikus.digital/quiz/static/locales/ru.json" } 
+        x <- Http.send $ get { url = "https://kurbikus.digital/quiz/static/locales/ru.json", headers = [] }
         pure $ PutNormalizeCss x
     Init -> batchEff model $ map pure [ JSTest ]
